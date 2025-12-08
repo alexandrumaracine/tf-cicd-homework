c324815a8e7e53117bc05ecfd46829b18b23dc97
Alexandru Maracine <alexandru.maracine@endava.com>
2025-12-08 12:28:11 +0200

    Set azurerm subscription_id explicitly

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,12 +1,5 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "azurerm" {
   features {}
+
+  subscription_id = "2685f674-ff40-4fa9-839d-ba6dcbaca8a1"
 }
