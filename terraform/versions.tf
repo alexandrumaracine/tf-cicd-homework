f3007989e27c3819ee7abda4b3391e4c7aac399a
Alexandru Maracine <alexandru.maracine@endava.com>
2025-12-09 09:54:26 +0200

    added new backend state for tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,3 +1,12 @@
 terraform {
-  required_version = ">= 1.5.0"
+  backend "local" {
+    path = "./tfstate/terraform.tfstate"
+  }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
 }
