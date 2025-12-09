823920dce871b188eaad846d3444bb277209d2f3
Alexandru Maracine <alexandru.maracine@endava.com>
2025-12-09 10:54:00 +0200

    replaced local backend with azure blob storage

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,6 +1,9 @@
 terraform {
-  backend "local" {
-    path = "./tfstate-remote/terraform.tfstate"
+  backend "azurerm" {
+    resource_group_name  = "testHomework"
+    storage_account_name = "tfstatealexmara"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
 
   required_providers {
